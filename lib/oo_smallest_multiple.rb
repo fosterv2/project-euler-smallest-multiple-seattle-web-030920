8f30e5ec56fa847f50cdadb07eb825684b288b40
fosterv2 <fosterv2@outlook.com>
2020-08-24 13:52:14 -0700

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+    def initialize(input)
+        @input = input
+    end
+
+    def lcm
+        lowest = 1
+        (2..@input).each do |num|
+            if lowest % num != 0
+                lowest *= num / gcd(lowest, num)
+            end
+        end
+        lowest
+    end
+
+    def gcd(a, b)
+        while b > 0
+            a %= b
+            return b if a == 0
+            b %= a
+        end
+        a
+    end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def gcd(a, b)
+    while b > 0
+        a %= b
+        return b if a == 0
+        b %= a
+    end
+    a
+end
+
+def smallest_multiple(input)
+    lowest = 1
+    (2..input).each do |num|
+        if lowest % num != 0
+            lowest *= num / gcd(lowest, num)
+        end
+    end
+    lowest
+end
